bff362ffad5d97aed73f10eb7c2edfd30153033c
Harsh <harshtanishq2002@gmail.com>
2024-07-28 14:16:21 +0530

    feat: rendered card component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css';
 import instance from "../ethereum/factory";
+import { CardGroup } from "semantic-ui-react";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -8,8 +10,20 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <CardGroup items={items} />;
+  }
+
   render() {
-    return <h3>{this.props.campaigns[0]}</h3>;
+    return <div>{this.renderCampaigns()}</div>;
   }
 }
 
