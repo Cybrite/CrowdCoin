d200b520442fd4694f8a81c2856af15bb21696c6
Harsh <harshtanishq2002@gmail.com>
2024-07-28 21:32:23 +0530

    feat: created form for new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
+import { Form, Button, FormField, Input } from "semantic-ui-react";
+import Layout from "../../components/Layout";
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: "",
+  };
+
   render() {
-    return <h1>Create a Campaign</h1>;
+    return (
+      <Layout>
+        <h3>Create a Campaign</h3>
+        <Form>
+          <FormField>
+            <label>Minimum Contribution</label>
+            <Input
+              label="Wei"
+              placeholder="Amount In Wei!"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={(event) =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
+          </FormField>
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
   }
 }
 
