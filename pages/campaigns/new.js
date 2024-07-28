0e1a5bbc403a6e01463d13f6d5da415fb99e7d82
Harsh <harshtanishq2002@gmail.com>
2024-07-28 21:12:45 +0530

    feat: added new page

---
+++ pages/campaigns/new.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+
+class CampaignNew extends Component {
+  render() {
+    return <h1>Create a Campaign</h1>;
+  }
+}
+
+export default CampaignNew;
--- pages/show.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function show () {
-    return <h1>This is the show page</h1>
-}
