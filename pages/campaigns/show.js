c61b04bb85f6aba11276ffa6c750995560c3e31d
Harsh <harshtanishq2002@gmail.com>
2024-07-30 19:04:33 +0530

    feat: created instance in show

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import Layout from "../../components/Layout";
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    console.log(props.query.address);
+
+    return { address: props.query.address };
+  }
+
   render() {
     return (
       <Layout>
         <h2>Show</h2>
       </Layout>
-    )
+    );
   }
 }
 
