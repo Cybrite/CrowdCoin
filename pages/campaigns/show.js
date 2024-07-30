8f8538690109952a51995106b4737c47afc53240
Harsh <harshtanishq2002@gmail.com>
2024-07-30 19:56:32 +0530

    feat: generating campaing data and using card semantic

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Layout from "../../components/Layout";
 import aboutcampaign from "../../ethereum/Campaign";
 import { CardGroup } from "semantic-ui-react";
+import web3 from "../../ethereum/web3";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -29,6 +30,29 @@ class CampaignShow extends Component {
         description:
           "The manager Created this campaign and can create requests to withdraw money",
         style: { overflowWrap: "break-word" },
+      },
+      {
+        header: String(minimumContribution),
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: String(requests),
+        meta: "Number of Requests by manager",
+        description:
+          "A request by the manager to withdraw money from the campaign",
+      },
+      {
+        header: String(contributors),
+        meta: "Number of Contributors",
+        description: "Number of people who have contributed to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend",
       },
     ];
 
