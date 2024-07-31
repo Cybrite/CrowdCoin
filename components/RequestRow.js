85faf1e7e40ff6e897c75591c5d5c4a86953de5a
Harsh <harshtanishq2002@gmail.com>
2024-07-31 18:26:34 +0530

    feat: minor tweakings

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,30 +1,56 @@
 import React, { Component } from "react";
-import { TableCell, TableRow, Button } from "semantic-ui-react";
+import { TableCell, TableRow, Button, Message } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
 import aboutcampaign from "../ethereum/Campaign";
 
 class RequestRow extends Component {
+  state = {
+    ApproveErrorMessage: "",
+    Approvloading: false,
+    FinalerrorMessage: "",
+    Finalloading: false,
+  };
+
   onApprove = async () => {
     const campaign = aboutcampaign(this.props.address);
-    const accounts = await web3.eth.getAccounts();
 
-    await campaign.methods.approveRequest(this.props.id).send({
-      from: accounts[0],
-    });
+    this.setState({ Approvloading: true });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await campaign.methods.approveRequest(this.props.id).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      this.setState({ ApproveErrorMessage: err.message });
+    }
+
+    this.setState({ Approvloading: false });
   };
 
   onFinalize = async () => {
     const campaign = aboutcampaign(this.props.address);
-    const accounts = await web3.eth.getAccounts();
 
-    await campaign.methods.finalizeRequest(this.props.id).send({
-      from: accounts[0],
-    });
+    this.setState({ Finalloading: true });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await campaign.methods.finalizeRequest(this.props.id).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      this.setState({ FinalerrorMessage: err.message });
+    }
+
+    this.setState({ Finalloading: false });
   };
 
   render() {
     // const readyToFinalize =
-    //   this.props.request.approvalCount > (this.props.approversCount / 2);
+    //   Number(this.props.request.approvalCount) >
+    //   Number(this.props.approversCount / 2);
 
     return (
       <TableRow
@@ -43,18 +69,34 @@ class RequestRow extends Component {
         </TableCell>
         <TableCell>
           {this.props.request.complete ? null : (
-            <Button color="green" basic onClick={this.onApprove}>
+            <Button
+              color="green"
+              basic
+              onClick={this.onApprove}
+              loading={this.state.Approvloading}
+            >
               Approve
             </Button>
           )}
+          {this.state.ApproveErrorMessage ? (
+            <Message negative content={this.state.ApproveErrorMessage} />
+          ) : null}
         </TableCell>
         <TableCell>
           {this.props.request.complete ? null : (
-            <Button color="teal" basic onClick={this.onFinalize}>
+            <Button
+              color="teal"
+              basic
+              onClick={this.onFinalize}
+              loading={this.state.Finalloading}
+            >
               Finalize
             </Button>
           )}
         </TableCell>
+        {this.state.FinalerrorMessage ? (
+          <Message negative content={this.state.FinalerrorMessage} />
+        ) : null}
       </TableRow>
     );
   }
